d6301091a7f7eff6c0ec973421f873b485cde0cc
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2025-01-09 13:26:35 +0100

    PNG make transparent for Contnet Switcher redesign

--- lib/features/Abfall_ABC/widgets/switcher_container.dart
+++ lib/features/Abfall_ABC/widgets/switcher_container.dart
@@ -15,19 +15,28 @@ class SwitcherContainer extends StatelessWidget {
       width: 140,
       height: 140,
       decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+        gradient: const LinearGradient(
+          colors: [Colors.blueAccent, Colors.greenAccent], // Farbverlauf
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(12),
+      ),
       child: Column(
         children: [
           Padding(
             padding: const EdgeInsets.only(top: 4.0, bottom: 4),
             child: ClipRRect(
                 borderRadius: BorderRadius.circular(15),
-                child: Image(
-                    height: 100,
-                    width: 100,
-                    image: AssetImage(
-                      picturepath,
-                    ))),
+                child: Container(
+                  color: Colors.transparent,
+                  child: Image(
+                      height: 90,
+                      width: 90,
+                      image: AssetImage(
+                        picturepath,
+                      )),
+                )),
           ),
           Text(text)
         ],
